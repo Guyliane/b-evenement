cba0cbda0bb6861cf0909d3cc5217da3c65774cb
Guyliane <guyliane@live.fr>
2022-07-13 15:41:31 +0200

    fix some animation useless

--- app.js
+++ app.js
@@ -16,16 +16,10 @@ const logoHome = document.querySelector(".logo-home");
 const imgBallon = document.querySelector(".img-ballon");
 const realisations = document.querySelectorAll(".realisations");
 
-gsap.from(logoHome, { y: 450, duration: 1, ease: "power4.out" });
+gsap.from(logoHome, { y: 450, duration: 1 });
 gsap.from(realisations, {
   y: 250,
   scrollTrigger: realisations,
-  duration: 1,
-  stagger: 0.1,
-});
-gsap.from(imgBallon, {
-  y: 250,
-  scrollTrigger: imgBallon,
   duration: 1,
   stagger: 0.1,
 });
