1548f87a77d3d1b1c8aff30c96b6dcc02f2d8f09
Guyliane <guyliane@live.fr>
2022-07-13 15:38:51 +0200

    add some animation with Greensock and srolltrigger library

--- app.js
+++ app.js
@@ -3,8 +3,29 @@
 const menuElementsToToggle = document.querySelectorAll(".toggle-menu");
 const iconeToggle = document.querySelector(".icone-toggle");
 
-const toggleMenu = () => menuElementsToToggle.forEach(el => el.classList.toggle("hidden"));
+const toggleMenu = () =>
+  menuElementsToToggle.forEach((el) => el.classList.toggle("hidden"));
 
 iconeToggle.addEventListener("click", toggleMenu);
 
 // ---------------------------------------
+
+// Greensock
+
+const logoHome = document.querySelector(".logo-home");
+const imgBallon = document.querySelector(".img-ballon");
+const realisations = document.querySelectorAll(".realisations");
+
+gsap.from(logoHome, { y: 450, duration: 1, ease: "power4.out" });
+gsap.from(realisations, {
+  y: 250,
+  scrollTrigger: realisations,
+  duration: 1,
+  stagger: 0.1,
+});
+gsap.from(imgBallon, {
+  y: 250,
+  scrollTrigger: imgBallon,
+  duration: 1,
+  stagger: 0.1,
+});
