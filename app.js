edb510bf75dc9be6308ec1c70ff846af3dd7a5e2
Guyliane <guyliane@live.fr>
2022-07-15 13:32:50 +0200

    Update autoplay prestation and page temoignage ignore

--- app.js
+++ app.js
@@ -47,6 +47,10 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
     dynamicBullets: "true",
   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
